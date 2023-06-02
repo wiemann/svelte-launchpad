ef490cfcfd33fc875e8e7e2b9de33f933f0ee083
Matthias Wiemann <mat@wiemann.name>
2023-06-02 09:56:35 +0200

    add form

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+import { superValidate } from 'sveltekit-superforms/server';
+import { fail } from '@sveltejs/kit';
+
+const schema = z.object({
+  name: z.string().optional(),
+  email: z.string().email()
+});
+
+export const load = async () => {
+  // Server API:
+  const form = await superValidate(schema);
+
+  // Always return { form } in load and form actions.
+  return { form };
+};
+
+
+export const actions = {
+  default: async ({ request }) => {
+    const form = await superValidate(request, schema);
+    console.log('POST', form);
+
+    // Convenient validation check:
+    if (!form.valid) {
+      // Again, always return { form } and things will just work.
+      return fail(400, { form });
+    }
+
+    // TODO: Do something with the validated data
+
+    // Yep, return { form } here too
+    return { form };
+  }
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,32 @@
+<script>
+	import { superForm } from 'sveltekit-superforms/client';
+	export let data;
+    const { form, errors, constraints } = superForm(data.form);
+
+
+</script>
+
 <h1 class="text-2xl">Svelte Launchpad</h1>
+
 <div class="my-4">
-    <p>This is the signup page to my great new project.</p>
+	<p>This is the signup page to my great new project.</p>
 </div>
 
+<div class="my-4 bg-green-200 p-2 rounded-lg">
+	<h2 class="text-xl mb-1">Receive an Invite when we launch</h2>
+
+	<form method="POST">
+		<div class="mb-2">
+			<input class="border px-2" 
+            type="email" 
+            name="email" 
+            bind:value={$form.email} 
+            placeholder="your@email.com"
+            data-invalid={$errors.email}
+            {...$constraints.email} />
+            {#if $errors.email}<span class="bg-red-700">{$errors.email}</span>{/if}
+		</div>
+
+		<div><button class="bg-green-800 text-white hover:bg-green-950 px-4 py-1 rounded">Signup</button></div>
+	</form>
+</div>
