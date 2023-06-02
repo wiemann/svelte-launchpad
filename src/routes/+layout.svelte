afe67e970edcc2174fbca764505d040d0c0879c7
Matthias Wiemann <mat@wiemann.name>
2023-06-02 09:35:33 +0200

    add tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+	import '../app.postcss';
+</script>
+<div class="max-w-xl mx-auto my-8">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-2xl">Svelte Launchpad</h1>
+<div class="my-4">
+    <p>This is the signup page to my great new project.</p>
+</div>
+
